cb457493b99dd9eef7d94030b1cffec2e75d7f2e
Shubhi Bhoora <45699032+sbhoora@users.noreply.github.com>
2022-05-29 10:44:21 -0400

    Update index.js

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { DashboardContainer, Logout, LogoutBtn, LogoutLinks } from './DashboardElements';
+import { DashboardContainer, Photo,Logout, LogoutBtn, LogoutLinks } from './DashboardElements';
 import { useAuth0 } from '@auth0/auth0-react';
+import logo from '/Users/shubhibhoora/fika_network/src/img/logo.png';
 
 
 const Dashboard = () => {
@@ -14,6 +15,8 @@ const Dashboard = () => {
           )}
         </LogoutBtn>
       </Logout>
+      <Photo><img src={logo} alt="Logo" /></Photo>
+      
     </DashboardContainer>
   )
 }
